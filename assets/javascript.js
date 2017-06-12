cfce11706613bc40da243ed67c78dc3023be0d0d
CyberAmigo <czavaleta93@gmail.com>
2017-06-12 16:03:19 -0500

    modified:   assets/css/custom.css  	modified:   assets/javascript.js  	modified:   src/about.html  	modified:   src/index.html

--- assets/javascript.js
+++ assets/javascript.js
@@ -2,8 +2,10 @@ $(window).scroll(function() {
   if ($('.navbar').offset().top > 50) {
     $('#custom-nav').addClass('affix');
     $('.navbar-fixed-top').addClass('top-nav-collapse');
+    $('.nav-item').addClass('nav-black-font');
   } else {
     $('#custom-nav').removeClass('affix');
     $('.navbar-fixed-top').removeClass('top-nav-collapse');
+    $('.nav-item').removeClass('nav-black-font');
   }
 });
